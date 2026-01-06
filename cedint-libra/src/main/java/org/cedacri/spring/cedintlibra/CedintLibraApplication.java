2574a92419a00c131c1060cfe2bdcd9f54c203d8
Djurinschi Andrei <andrei.djurinschi@iongroup.com>
2026-01-06 12:20:30 +0200

    init repo

---
+++ cedint-libra/src/main/java/org/cedacri/spring/cedintlibra/CedintLibraApplication.java
@@ -0,0 +1,13 @@
+package org.cedacri.spring.cedintlibra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedintLibraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CedintLibraApplication.class, args);
+    }
+
+}
---
+++ cedint-libra/src/test/java/org/cedacri/spring/cedintlibra/CedintLibraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cedacri.spring.cedintlibra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedintLibraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
